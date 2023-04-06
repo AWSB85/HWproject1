5214a6d8800031428927a765b454dd3bd41609c8
awsbasiya <awsbasiya@localhost>
2023-04-06 16:59:55 -0400

    class1 commit

---
+++ src/Demo1/Class1.java
@@ -0,0 +1,15 @@
+package Demo1;
+
+public class Class1 {
+	
+public static void main (String [] args) {
+		
+		System.out.println("Class1........");
+		
+		System.out.println("Git Class1.....");
+		
+		
+	}
+
+
+}
